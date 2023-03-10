bb5b24d8d954479e8a7797c048d8ee5a0528abc8
pjagiter <kwasniewiczl@pjwstk.edu.pl>
2023-03-10 16:51:56 +0100

    Revert "add new script,"
    This reverts commit 2ee62d0d376da75d753d5b7f1223147b74761d88.

--- main.py
+++ main.py
@@ -8,8 +8,7 @@ import requests
 pageurl = 'https:github.com'
 #pageurl = input("podaj adres strony")
 #date = input("podaj date")
-#zmiana
-date = 20180101
+date = 20150101
 url ="http://archive.org/wayback/available?url="+pageurl+"&timestamp="+str(date)
 response = requests.get(url)
 d = response.json()
